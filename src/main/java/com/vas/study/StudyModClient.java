239a791006ba4fd5eb5096e836a4d91c13c798b3
Vastosine <2261819266@qq.com>
2026-08-06 09:13:42 +0800

    build

---
+++ src/main/java/com/vas/study/StudyModClient.java
@@ -0,0 +1,10 @@
+package com.vas.study;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StudyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/vas/study/StudyModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.vas.study;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StudyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
